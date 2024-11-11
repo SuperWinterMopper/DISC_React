2dbe1d3aa6803878410488f76968855aa4ab88e1
Acton Healy <afkhealy@gmail.com>
2024-11-10 18:38:27 -0600

    completed banner for profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -24,6 +24,35 @@ export default function Profile() {
     )
   }
 
+  function statistic(props) {
+    return (
+      <div className='statistic'>
+        <h3>{props.num}</h3>
+        <h4>{props.type}</h4>
+      </div>
+    )
+  }
+
+  function profileBannerInfo(props) {
+    return (    
+      <div className='profileInfoContainer'>
+        <div className='profilePicture'>
+          <img src='https://resources.tidal.com/images/511f57cc/b5db/45f6/b132/3f1a6f2f627a/640x640.jpg'></img>
+        </div>
+        <div className='textInfo'>
+          <h1>Ethan Pineda</h1>
+          <h2>@themarias@gmail.com</h2>
+          <div className='statisticsField'>
+            {statistic({num: 40, type: "Tags"})}
+            {statistic({num: 28, type: "Following"})}
+            {statistic({num: 23, type: "Followers"})}
+          </div>
+          <p>Junior at Northwestern University Studying Mechanical Engineering.</p>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="main-body">
     <section className='desktopSectionProfile'>
@@ -34,6 +63,7 @@ export default function Profile() {
           {navbarButton({ text: "Profile", icon : <ProfileIcon />, to : "/Profile"})}
           {navbarButton({ text: "Message", icon : <MessageIcon />, to : "/Message"})}
         </nav-bar>
+        {profileBannerInfo()}
       </div>
       <footer>
         <h5>Spirit</h5>
